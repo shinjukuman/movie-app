49418dbc39c92b77482d2707fea4ba2804db4895
chrisgardner402 <chojh3308@gmail.com>
2021-01-27 12:07:38 +0900

    2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Potato from './Potato';
+
+// function Food(props) {
+//   return (<h1>I Like {props.fav}</h1>);
+// }
+
+function Food({ fav }) {
+  return (<h1>I Like {fav}</h1>);
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Potato() {
-    return (
-        <h4>I Love Potatoes</h4>
-    );
-}
-
-export default Potato;
